e81a26c6fe3ddf7b32142350e5583c94f54a50fd
Ferosh <52982404+ferodevs@users.noreply.github.com>
2022-06-29 17:20:25 -0700

    feat: lets have some fun...

---
+++ api/uuid.js
@@ -0,0 +1,12 @@
+function uuid () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+export default function handler(request, response) {
+    response.status(200)
+    response.json({ data: uuid() });
+}
